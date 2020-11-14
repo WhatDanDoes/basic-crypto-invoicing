367a402e5a531ec83abe71620bffdc97a06486b4
Daniel Bidulock <daniel@capitolhill.ca>
2020-11-14 20:45:55 +0000

    Create QR from wallet address

--- routes/invoice.js
+++ routes/invoice.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 const cw = require('crypto-wallets');
+const qrcode = require('qrcode');
 
 /**
  * GET /invoice/:id
  */
 router.get('/:id', (req, res, next) => {
   models.Invoice.findOne({ _id: req.params.id}).then(invoice => {
-    res.render('invoice/show', { invoice: invoice, messages: req.flash() });
+
+    qrcode.toDataURL('I am a pony!', (err, url) => {
+      if (err) {
+        req.flash('error', err.message);
+        return res.redirect('/');
+      }
+      res.render('invoice/show', { invoice: invoice, messages: req.flash(), qr: url });
+    })
   }).catch(err => {
     req.flash('error', err.message);
     res.redirect('/');
