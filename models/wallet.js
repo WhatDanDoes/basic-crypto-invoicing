1d671a490985cd3a80066a83b5beefde078542cf
Daniel Bidulock <daniel@capitolhill.ca>
2020-11-14 16:38:01 +0000

    Fill in wallet properties

--- models/wallet.js
+++ models/wallet.js
@@ -4,6 +4,24 @@ module.exports = function(mongoose) {
   const Schema = mongoose.Schema;
 
   const WalletSchema = new Schema({
+    symbol: {
+      type: String,
+      trim: true,
+      required: [true, 'No symbol supplied'],
+      empty: [false, 'No symbol supplied'],
+    },
+    publicKey: {
+      type: String,
+      trim: true,
+      required: [true, 'No public address supplied'],
+      empty: [false, 'No public address supplied'],
+    },
+    privateKey: {
+      type: String,
+      trim: true,
+      required: [true, 'No private key supplied'],
+      empty: [false, 'No private key supplied'],
+    },
   }, {
     timestamps: true
   });
