ac8dd85ae901fc527d19e33d8b4e05a8d70e8048
Daniel Bidulock <daniel@capitolhill.ca>
2020-11-14 16:33:43 +0000

    Sketch in validation tests

--- spec/models/walletSpec.js
+++ spec/models/walletSpec.js
@@ -7,7 +7,11 @@ describe('Wallet', () => {
   let wallet;
 
   beforeEach(done => {
-    wallet = new Wallet({});
+    wallet = new Wallet({
+      symbol: 'BTC',
+      publicKey: 'some-pseudo-random-public-address',
+      privateKey: 'some-pseudo-random-private-key',
+    });
     done();
   });
 
@@ -29,6 +33,42 @@ describe('Wallet', () => {
         done();
       }).catch(err => {
         done.fail(err);
+      });
+    });
+
+    it('requires a symbol', done => {
+      wallet.symbol = undefined;
+
+      wallet.save().then(obj => {
+        done.fail('This should not have saved');
+      }).catch(error => {
+        expect(Object.keys(error.errors).length).toEqual(1);
+        expect(error.errors['symbol'].message).toEqual('No symbol supplied');
+        done();
+      });
+    });
+
+    it('requires a public address', done => {
+      wallet.publicKey = undefined;
+
+      wallet.save().then(obj => {
+        done.fail('This should not have saved');
+      }).catch(error => {
+        expect(Object.keys(error.errors).length).toEqual(1);
+        expect(error.errors['publicKey'].message).toEqual('No public address supplied');
+        done();
+      });
+    });
+
+    it('requires a private key', done => {
+      wallet.privateKey = undefined;
+
+      wallet.save().then(obj => {
+        done.fail('This should not have saved');
+      }).catch(error => {
+        expect(Object.keys(error.errors).length).toEqual(1);
+        expect(error.errors['privateKey'].message).toEqual('No private key supplied');
+        done();
       });
     });
   });
